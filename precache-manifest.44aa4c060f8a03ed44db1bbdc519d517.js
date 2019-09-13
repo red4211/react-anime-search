8372f3b171372eacc5921e635281587166be88e1
red42 <mperepelitsa421@gmail.com>
2019-09-13 16:20:40 +0300

    Updates

---
+++ precache-manifest.44aa4c060f8a03ed44db1bbdc519d517.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7515e4a09b756f4a489f1e5927c28c",
+    "url": "/react-anime-search/index.html"
+  },
+  {
+    "revision": "3d32c62728da4bb4b9b2",
+    "url": "/react-anime-search/static/css/main.4bf9ea08.chunk.css"
+  },
+  {
+    "revision": "f478c3e6a5a59dee73f6",
+    "url": "/react-anime-search/static/js/2.81f98875.chunk.js"
+  },
+  {
+    "revision": "3d32c62728da4bb4b9b2",
+    "url": "/react-anime-search/static/js/main.d7eeeb2f.chunk.js"
+  },
+  {
+    "revision": "d22cd1a8c98570cb36c3",
+    "url": "/react-anime-search/static/js/runtime~main.439cf2f8.js"
+  }
+]);
